44b0c04a08feee475eec10a06c99c6b96259f6c9
JieFeng <fengjie1102@gmail.com>
2018-12-22 22:28:46 +0800

    P389-2完成

--- P389_P390.h
+++ P389_P390.h
@@ -6,10 +6,18 @@
 @Description: The twelfth week's assignment of data structure of Mr. Jianhua Lv
 **************************************************/
 
+#include "P340.h"	//引入自定义的Graph类
+
 /*
 以下对应于英文书P389 2
+答：见word文档
 */
+
 
 /*
 以下对应于英文书P390 5
 */
+class TopoIterator
+{
+
+};
