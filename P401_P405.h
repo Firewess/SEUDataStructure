c28fdfba5eebc88301e600935e302f0b50987eb0
JieFeng <fengjie1102@gmail.com>
2018-12-23 09:57:25 +0800

    P401-1完成

--- P401_P405.h
+++ P401_P405.h
@@ -6,13 +6,24 @@
 @Description: The twelfth week's assignment of data structure of Mr. Jianhua Lv
 **************************************************/
 
+#include "P194.h"	//引入自定义的Chain模板类
+
 /*
 以下对应于英文书P401 1
+答：见P401-1.docx
 */
 
 /*
 以下对应于英文书P401 3
 */
+template<class T>
+void LinklistInsertSort(Chain<T> chain)
+{
+	for (int i = 2; i < chain.length(); i++)
+	{
+		
+	}
+}
 
 /*
 以下对应于英文书P405 1
@@ -25,3 +36,23 @@
 /*
 以下对应于英文书P405 5
 */
+
+void run_P401_P405()
+{
+	Chain<int> chain;
+	chain.Add(0);	//因为需要保证第一个元素是最小的
+	chain.Add(2);
+	chain.Add(16);
+	chain.Add(30);
+	chain.Add(8);
+	chain.Add(28);
+	chain.Add(4);
+	chain.Add(10);
+	chain.Add(20);
+	chain.Add(6);
+	chain.Add(18);
+
+	cout << "排序之前的链表：\n" << chain << endl;
+	LinklistInsertSort(chain);
+	cout << "排序之后的链表: \n" << chain << endl;
+}
