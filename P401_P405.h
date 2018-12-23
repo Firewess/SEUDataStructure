d4c4f281a29533ee3bbe5dbe2922f1ae2f7200ff
JieFeng <fengjie1102@gmail.com>
2018-12-23 13:12:22 +0800

    12周作业：P401-3完成

--- P401_P405.h
+++ P401_P405.h
@@ -6,7 +6,155 @@
 @Description: The twelfth week's assignment of data structure of Mr. Jianhua Lv
 **************************************************/
 
-#include "P194.h"	//引入自定义的Chain模板类
+//以下是为了便利而引入的Chain模板类的定义
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/*
+以下对应于英文书P194 3
+*/
+template<class T> class Chain;
+template<class T> class ChainIterator;
+template<class T> class Test;
+
+template<class T>
+class ChainNode
+{
+	friend class Chain<T>;
+	friend class ChainIterator<T>;
+	friend class Test<T>;
+public:
+	ChainNode(T element = 0, ChainNode<T> *next = 0)
+	{
+		data = element;
+		link = next;
+	}
+
+	T GetData()
+	{
+		return data;
+	}
+
+	ChainNode<T> * GetNext()
+	{
+		return link;
+	}
+
+private:
+	T data;
+	ChainNode<T> *link;
+};
+
+template<class T>
+class Chain
+{
+public:
+	Chain()
+	{
+		first = 0;
+	}
+	~Chain()
+	{
+		ChainNode<T> *temp = first;
+		while (first)
+		{
+			temp = first;
+			first = first->link;
+			delete temp;
+		}
+	}
+
+	void Add(T element);
+
+	int length();
+	void Delete(ChainNode<T> *x);
+
+	template<class T>
+	friend ostream & operator <<(ostream &os, Chain<T> &linkList);
+
+	ChainNode<T> * GetFirst()
+	{
+		return first;
+	}
+
+	void setFirst(ChainNode<T> *newFirst)	//为P201.h实现链式模板队列
+	{
+		first = newFirst;
+	}
+
+	ChainIterator<T> begin() { return ChainIterator<T>(first); }
+	ChainIterator<T> end() { return ChainIterator<T>(0); }
+
+private:
+	ChainNode<T> *first;
+};
+
+template<class T>
+void Chain<T>::Add(T element)
+{
+	if (first == 0)
+	{
+		first = new ChainNode<T>(element);
+		return;
+	}
+	ChainNode<T> *temp = new ChainNode<T>(element);
+	ChainNode<T> *p = first;
+	while (p->link != 0)
+	{
+		p = p->link;
+	}
+	p->link = temp;
+}
+
+template<class T>
+void Chain<T>::Delete(ChainNode<T> *x)
+{
+	if (x == first)
+	{
+		first = first->link;
+		delete x;
+		return;
+	}
+	ChainNode<T> *temp = first;
+	while (temp)
+	{
+		if (temp->link == x)
+			break;
+		temp = temp->link;
+	}
+	temp = temp->link->link;
+	delete x;
+}
+
+template<class T>
+int Chain<T>::length()
+{
+	ChainNode<T> *temp = first;
+	int count = 0;
+	while (temp)
+	{
+		count++;
+		temp = temp->link;
+	}
+	return count;
+}
+
+template<class T>
+ostream & operator <<(ostream &os, Chain<T> &linkList)
+{
+	os << "当前链表存储情况为：";
+	ChainNode<T> *temp = linkList.first;
+	while (linkList.first)
+	{
+		os << linkList.first->GetData() << " ";
+		linkList.first = linkList.first->GetNext();
+	}
+	linkList.first = temp;	//把头指针归位
+
+	return os;
+}
 
 /*
 以下对应于英文书P401 1
@@ -17,30 +165,66 @@
 以下对应于英文书P401 3
 */
 template<class T>
-void LinklistInsertSort(Chain<T> chain)
+class Test
 {
-	for (int i = 2; i < chain.length(); i++)
+private:
+	Test();
+
+public:
+	static void LinklistInsertSort(Chain<T> &chain)
 	{
-		
+		ChainNode<T> *before = chain.GetFirst();
+		ChainNode<T> *temp = chain.GetFirst()->GetNext();
+		ChainNode<T> *last = chain.GetFirst()->GetNext();
+		ChainNode<T> *curr = temp->GetNext();
+
+		bool changed = false;
+		for (int i = 2; i < chain.length(); i++)
+		{
+			while (temp != curr)
+			{
+				if (temp->GetData() > curr->GetData())
+				{
+					before->link = curr;
+					last->link = curr->GetNext();
+					curr->link = temp;
+					curr = last;
+					changed = true;
+					break;
+				}
+				before = before->GetNext();
+				temp = temp->GetNext();
+			}
+			if (changed == false) last = last->GetNext();
+			else changed = false;
+			before = chain.GetFirst();
+			temp = chain.GetFirst()->GetNext();
+			curr = curr->GetNext();
+		}
 	}
-}
+};
 
 /*
 以下对应于英文书P405 1
+答：见P405.docx
 */
 
 /*
 以下对应于英文书P405 2
+答：见P405.docx
 */
 
 /*
 以下对应于英文书P405 5
+答：见P405.docx
 */
+
 
 void run_P401_P405()
 {
+	//以下对应于英文书P401 3
 	Chain<int> chain;
-	chain.Add(0);	//因为需要保证第一个元素是最小的
+	chain.Add(0);	//为了避免处理头节点的问题，这里保证第一个元素最小
 	chain.Add(2);
 	chain.Add(16);
 	chain.Add(30);
@@ -53,6 +237,6 @@ void run_P401_P405()
 	chain.Add(18);
 
 	cout << "排序之前的链表：\n" << chain << endl;
-	LinklistInsertSort(chain);
+	Test<int>::LinklistInsertSort(chain);
 	cout << "排序之后的链表: \n" << chain << endl;
 }
