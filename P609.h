34f57e70e028d427f1eb8e30f1b8d37e41460571
JieFeng <fengjie1102@gmail.com>
2018-12-30 15:27:50 +0800

    修改第15周作业

--- P609.h
+++ P609.h
@@ -5,3 +5,7 @@
 @File: P609.h
 @Description: The fifteenth week's assignment of data structure of Mr. Jianhua Lv
 **************************************************/
+
+/*
+以下对应于英文书P609 3
+*/
--- P623.h
+++ P623.h
@@ -5,3 +5,7 @@
 @File: P623.h
 @Description: The fifteenth week's assignment of data structure of Mr. Jianhua Lv
 **************************************************/
+
+/*
+以下对应于英文书P623 2或者4，哪个都行
+*/
